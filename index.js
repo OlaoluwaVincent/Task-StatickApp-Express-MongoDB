c908540faa9d69f20f596bb2df2d12d6b9a80f69
olaoluwa vincent <olaoluwavincent@gmail.com>
2023-03-05 06:10:32 +0100

    again

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const tasksRoutes = require('./routes/tasks');
 
 const connectDB = require('./db/connect');
